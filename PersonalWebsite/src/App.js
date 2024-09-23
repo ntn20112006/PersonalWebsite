20d3667c16defb8f450d9f9a27bc197768d7d5fd
Tri Nghia Nguyen <tringhianguyen@Tris-MacBook-Air.local>
2024-09-23 16:13:45 -0400

    Add Footer Template

--- PersonalWebsite/src/App.js
+++ PersonalWebsite/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import NavBar from './NavBar';
 import AboutMeContent from './AboutMeContent';
+import ContactBar from './ContactBar';
 
 function App() {
   return (
     <div>
       <NavBar/>
       <AboutMeContent/>
+      <ContactBar/>
     </div>
   );
 }
