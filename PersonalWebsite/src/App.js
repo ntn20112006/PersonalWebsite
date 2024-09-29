3107f793a82ce703b5e4487eb98ae830a586c91d
Tri Nghia Nguyen <tringhianguyen@Tris-MacBook-Air.local>
2024-09-29 18:37:20 -0400

    Add navigation

--- PersonalWebsite/src/App.js
+++ PersonalWebsite/src/App.js
@@ -1,8 +1,22 @@
-import AboutMePage from './AboutMePage';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import AboutMePage from './AboutMePage'
+import InterestsPage from './InterestsPage'
+
 
 function App() {
   return (
-    <AboutMePage/>
+    <Router>
+      <Routes>
+        <Route path='/' element={<AboutMePage/>} />
+        <Route path='/AboutMePage' element={<AboutMePage/>} />
+        <Route path='/InterestsPage' element={<InterestsPage/>} />
+      </Routes>
+    </Router>
   );
 }
 
