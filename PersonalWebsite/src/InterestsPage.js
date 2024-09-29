ffb030ff88d3231a8b81ed86197b16da8b04e5f5
Tri Nghia Nguyen <tringhianguyen@Tris-MacBook-Air.local>
2024-09-29 18:00:48 -0400

    Add interests page

---
+++ PersonalWebsite/src/InterestsPage.js
@@ -0,0 +1,13 @@
+import NavBar from './NavBar';
+import ContactBar from './ContactBar';
+
+function InterestsPage() {
+    return (
+        <div>
+            <NavBar/>
+            <ContactBar/>
+        </div>
+    );
+}
+
+export default InterestsPage;
