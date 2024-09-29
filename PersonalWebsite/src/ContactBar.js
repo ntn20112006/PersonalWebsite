19c0439f8aac90e84b6f85ffa75127951d2e9ef4
Tri Nghia Nguyen <tringhianguyen@Tris-MacBook-Air.local>
2024-09-29 17:18:04 -0400

    Add info for footer

--- PersonalWebsite/src/ContactBar.js
+++ PersonalWebsite/src/ContactBar.js
@@ -4,29 +4,53 @@ import {
     MDBCol,
     MDBRow
 } from 'mdb-react-ui-kit';
+import { ReactComponent as GitHubIcon } from './images/githubIcon.svg';
+import { ReactComponent as LinkedInIcon } from './images/linkedinIcon.svg';
+import React from 'react';
+
 
 function ContactBar() {
+    const githubIconClicked = () =>{ 
+        window.location.href = 'https://github.com/ntn20112006';
+    }
+
+    //Remember to change link
+    const linkedinIconClicked = () =>{ 
+        window.location.href = 'https://www.example.com';
+    }
+
     return (
         <MDBFooter bgColor='light' className='text-center text-lg-left'>
-            <MDBContainer className='p-4'>
+            <MDBContainer className='p-2'>
                 <MDBRow>
-                    <MDBCol lg='6' md='12' className='mb-4 mb-md-0'>
-                        <h5 className='text-uppercase'>Footer text</h5>
+                    <MDBCol lg='4' md='12' className='mb-4 mb-md-0'>
+                        <h5>Phone</h5>
                         <p>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis molestias.
-                            Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam voluptatem veniam,
-                            est atque cumque eum delectus sint!
+                            (647) 859-4536
                         </p>
-                        </MDBCol>
+                    </MDBCol>
 
-                        <MDBCol lg='6' md='12' className='mb-4 mb-md-0'>
-                        <h5 className='text-uppercase'>Footer text</h5>
-
+                    <MDBCol lg='4' md='12' className='mb-4 mb-md-0'>
+                        <h5>Email</h5>
                         <p>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis molestias.
-                            Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam voluptatem veniam,
-                            est atque cumque eum delectus sint!
+                            ntn20112006@gmail.com
                         </p>
+                    </MDBCol>
+
+                    <MDBCol lg='4' md='12' className='mb-4 mb-md-0'>
+                        <h5>Follow Me</h5>
+                        <MDBRow className='mb-4 mb-md-0'>
+                            <MDBCol lg='6' md='6' className='mb-4 mb-md-0'>
+                                <div onClick={githubIconClicked} style={{textAlign: 'right'}}>
+                                    <GitHubIcon/>
+                                </div>
+                            </MDBCol>
+                            <MDBCol lg='6' md='6' className='mb-4 mb-md-0'>
+                                <div onClick={linkedinIconClicked} style={{textAlign: 'left'}}>
+                                    <LinkedInIcon/>
+                                </div>
+                            </MDBCol>
+                        </MDBRow>
                     </MDBCol>
                 </MDBRow>
             </MDBContainer>
